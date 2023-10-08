62c7e0a8f3dfc2f38054dee6976754038edc38fd
MrNightFury <73826973+MrNightFury@users.noreply.github.com>
2023-10-08 12:55:57 +0300

    created game.js

---
+++ js/game.js
@@ -0,0 +1,25 @@
+const STATE = {
+    NONE: -1,
+    ZERO: 0,
+    CROSS: 1
+}
+
+var currentPlayer = 'x'; // 'x' or 'o'
+
+var curField;
+
+function clearField() {
+
+}
+
+function turnEnd() {
+
+}
+
+function changePlayer() {
+
+}
+
+function checkWin(field) {
+
+}
