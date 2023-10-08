71f87f887368631707804f91e76025f49fcfa775
sergkhr <serg.khrechko@gmail.com>
2023-10-08 13:45:13 +0300

    made turn check and unit tests

--- js/input.js
+++ js/input.js
@@ -1,3 +1,34 @@
 $(".field").click(function() {
     $(this).addClass("active");
 });
+
+function isCrossTurn(state){
+    if(state == 'x'){
+        return true;
+    }
+    if(state == 'o'){
+        return false;
+    }
+    console.error("Invalid state: " + state);
+    return false; 
+}
+
+function unitTest_isCrossTurn(){
+    console.log("unitTest_isCrossTurn");
+    console.assert(isCrossTurn('x') == true, "isCrossTurn('x') == true");
+    console.assert(isCrossTurn('o') == false, "isCrossTurn('o') == false");
+    console.assert(isCrossTurn('') == false, "isCrossTurn('') == false");
+    console.log("unitTest_isCrossTurn ended");
+}
+unitTest_isCrossTurn();
+
+
+function placeCross(field){
+
+}
+
+function placeZero(field){
+    
+}
+
+
