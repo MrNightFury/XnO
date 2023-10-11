ac41499c5a0baa18414101cb2093d9240f603bd9
MrNightFury <73826973+MrNightFury@users.noreply.github.com>
2023-10-11 11:23:31 +0300

    Finally BDD for displayResult

--- features/displayResult.feature
+++ features/displayResult.feature
@@ -5,3 +5,18 @@ Feature: Display Result
         Given Page is open
         When Needs to show Cross win
         Then Cross win is displayed
+
+    Scenario: Showing Zero Win
+        Given Page is open
+        When Needs to show Zero win
+        Then Zero win is displayed
+
+    Scenario: Showing Draw
+        Given Page is open
+        When Needs to show Draw
+        Then Draw is displayed
+
+    Scenario: Error
+        Given Page is open
+        When Incorrect state presented
+        Then Error is displayed
--- features/displayResultSteps.js
+++ features/displayResultSteps.js
@@ -4,7 +4,7 @@ const { Builder, By, Key } = require("selenium-webdriver");
 
 const driver = new Builder().forBrowser('firefox').build();
 
-Given("Page is open", { timeout: 100000 }, async () => {
+Given("Page is open", { timeout: 10000 }, async () => {
     await driver.get('http://127.0.0.1:5500/views/index.html');
 });
 
@@ -12,9 +12,39 @@ When("Needs to show Cross win", async () => {
     await driver.executeScript("displayResult(STATE.CROSS)");
 })
 
+When("Needs to show Zero win", async () => {
+    await driver.executeScript("displayResult(STATE.ZERO)");
+})
+
+When("Needs to show Draw", async () => {
+    await driver.executeScript("displayResult(STATE.DRAW)");
+})
+
+When("Incorrect state presented", async () => {
+    await driver.executeScript("displayResult(10000)");
+})
+
 Then("Cross win is displayed", async () => {
     await driver.findElement(By.id("winner")).getText().then(res => {
         assert.equal(res, "Cross is winner!");
+    })
+})
+
+Then("Zero win is displayed", async () => {
+    await driver.findElement(By.id("winner")).getText().then(res => {
+        assert.equal(res, "Zero is winner!");
+    })
+})
+
+Then("Draw is displayed", async () => {
+    await driver.findElement(By.id("winner")).getText().then(res => {
+        assert.equal(res, "Draw!");
+    })
+})
+
+Then("Error is displayed", async () => {
+    await driver.findElement(By.id("winner")).getText().then(res => {
+        assert.equal(res, "Incorrect state");
     })
 })
 
